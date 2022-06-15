bb2936af04b4db001d4038d96e2984e3fda972e4
Md. Mahfuj Hasan <mahfuj170142@diit.edu.bd>
2022-06-15 19:30:13 +0600

    mongoDB added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4kpep.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -13,7 +14,23 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log("ggggggggg")
+        const database = client.db('home_refinement');
+        const appointmentsCollection = database.collection('appointments');
+
+        app.get('appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocalDateString();
+            const query = { email: email, date: date }
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            res.json(result);
+        });
     }
     finally {
         // await client.close();
